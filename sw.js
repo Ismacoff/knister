9c058a41ed33814ad25955be07730b4504b549b2
Ismacoff <arndt.hsv@gmail.com>
2026-09-20 19:52:39 +0200

    Cache premium app icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='knister-v1';
-const APP=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='knister-v2';
+const APP=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/knister-premium-192.png','./icons/knister-premium-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
 self.addEventListener('fetch',event=>{
